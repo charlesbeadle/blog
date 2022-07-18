3e33d3532459824e40fab41019cdae115f6c2d0e
Charles Beadle <charles.beadle@protonmail.com>
2022-07-18 17:39:59 -0400

    Replace Mix with Vite

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,16 @@
   <meta name="author" content="Charles Beadle"> 
   <meta name="description" content="Linux System Administration, Web Development, and More">
   <meta name="format-detection" content="telephone=no">
-  <link rel="stylesheet" href="/css/app.css">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="stylesheet" href="/css/app.css">  
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
   <link rel="manifest" href="/site.webmanifest">
   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="theme-color" content="#ffffff">
   <title>Charles Beadle's Tech Blog</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])  
 </head>
 <body class="bg-gradient-to-b from-thedeep to-[#000] text-moon min-h-screen flex flex-col justify-between font-mono">
   <!--
@@ -83,7 +84,5 @@
     </main>
   </div>
   <x-footer/>
-  
-  <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
