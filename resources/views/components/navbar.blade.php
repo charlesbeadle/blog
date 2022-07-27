251cb12cf30f6b7ccb5b3fcd2ff3930ff196342a
Charles Beadle <charles.beadle@protonmail.com>
2022-07-27 16:53:38 -0400

    Revert nav link color back to white

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="mb-14">
     <div class="mx-auto py-4 sm:py-8 w-11/12 max-w-screen-2xl md:w-[69%] lg:w-[55%] xl:w-1/2 2xl:w-2/3 flex items-center justify-start lg:justify-end">
-      <a href="/" class="border-thedeep text-lg text-moon font-bold hover:border-moon border-b-2 transition-colors duration-200">HOME</a>
+      <a href="/" class="border-thedeep text-lg text-glare font-bold hover:border-glare border-b-2 transition-colors duration-200">HOME</a>
     </div>
   </nav>
 </header>
